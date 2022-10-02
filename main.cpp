6e645031aa5d52f3dad8f2d164f161ddd9cf5f4d
KimShelt <100987688+KimShelt@users.noreply.github.com>
2022-10-02 19:50:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,40 +6,45 @@
 using namespace std;
 bool isValid(const wstring& s)
 {
-    for(auto c:s)
-        if (!iswalpha(c) || !iswupper(c))
+    for (auto c:s)
+        if (!isalpha(c) || !isupper(c))
             return false;
     return true;
 }
-int main(int argc, char **argv)
+
+int main()
 {
-    int key=3;
     locale loc("ru_RU.UTF-8");
     locale::global(loc);
-    wstring z;
+    int key;
     wstring text;
-    unsigned pa;
-    wcout<<L"Cipher ready. Input Password: ";
-    wcin>>z;
+    int op;
+    wcout<<L"Cipher ready. Input key: ";
+    wcin>>key;
+    if (!wcin.good()) {
+        wcerr<<L"Key not valid\n";
+        return 0;
+    }
+
     modBetaCipher cipher(key);
     do {
         wcout<<L"Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
-        wcin>>pa;
-        if (pa > 2) {
+        wcin>>op;
+        if (op > 2) {
             wcout<<L"Wrong operation\n";
-        } else if (pa >0) {
+        } else if (op >0) {
             wcout<<L"Cipher ready. Input text: ";
             wcin>>text;
             if (isValid(text)) {
-                if (pa==1) {
-                    wcout<<L"Encrypted text: "<<cipher.CoderBetaCipher(cipher, text)<<endl;
+                if (op==1) {
+                    wcout<<L"Encrypted text: "<<cipher.encrypt(text)<<endl;
                 } else {
-                    wcout<<L"Decrypted text: "<<cipher.DecoderBetaCipher(cipher, text)<<endl;
+                    wcout<<L"Decrypted text: "<<cipher.decrypt(text)<<endl;
                 }
             } else {
                 wcout<<L"Operation aborted: Invalid text\n";
             }
         }
-    } while (pa!=0);
+    } while (op!=0);
     return 0;
 }
